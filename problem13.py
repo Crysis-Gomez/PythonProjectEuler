a4cd0ece675f0672d4b6ac7880e426b43347b382
Crysis-Gomez <Crysis.gomez@gmail.com>
2013-09-11 13:29:59 +0200

    added some changes

--- problem13.py
+++ problem13.py
@@ -1,12 +1,16 @@
 import time
 import math
+from urllib.request import urlopen
+import urllib.request
+
+localFile = open('file.html', 'w')
 
 class largeSum:
 
     def __init__(self):
         self.number_string = ""
         self.start()
-                
+    
     def start(self):
         t1 = time.clock()
         self.calculate()
@@ -15,15 +19,15 @@ class largeSum:
         print('Time: %0.6f' % (t2-t1))
 
     def calculate(self):
-        f = open('euler.txt')
-        lines = f.readlines()
+        u = urlopen('https://raw.github.com/Crysis-Gomez/PythonProjectEuler/master/euler.txt')        
+        lines = u.readlines()
         arrayList = []
         
         for l in range(0,len(lines)):
             arrayList.append(int(lines[l]))
                 
         totalSum = sum(arrayList)
-        f.close()
+        u.close()
         for x in range(0,10):
             self.number_string += str(totalSum)[x]
    
